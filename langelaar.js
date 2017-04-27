6e0c10103a6ff705a2bd59149299bf47bec64fd1
nickshch <ndnd@mail.ru>
2017-04-27 22:21:53 +0300

    Dimensional array func to js file

--- langelaar.js
+++ langelaar.js
@@ -1,15 +1,3 @@
-Array.matrix = function(numrows, numcols, initial) {
-    var arr = [];
-    for (var i = 0; i < numrows; ++i) {
-        var columns = [];
-        for (var j = 0; j < numcols; ++j) {
-            columns[j] = initial;
-        }
-        arr[i] = columns;
-    }
-    return arr;
-}
-
 function rgbToHsv() {
   var width = img.width;
   var height = img.height;
